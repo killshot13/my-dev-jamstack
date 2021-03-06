040c33737e6be17604d1103c79a5c8711806a040
killshot13 <dmreh@outlook.com>
2021-03-06 10:16:35 -0600

    npm & theme updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,40 +8,40 @@
 // use initialized flag to ensure that window.onGatsbyPreRouteUpdate() and
 // window.onGatsbyRouteUpdate() will not be called before
 // window.onGatsbyInitialClientRender() has run
-let initialized = false;
+let initialized = false
 
 exports.onInitialClientRender = () => {
-  initialized = true;
-  if (
-    "onGatsbyInitialClientRender" in window &&
-    typeof window.onGatsbyInitialClientRender === "function"
-  ) {
-    window.onGatsbyInitialClientRender();
-  }
-  if (
-    "onGatsbyRouteUpdate" in window &&
-    typeof window.onGatsbyRouteUpdate === "function"
-  ) {
-    window.onGatsbyRouteUpdate();
-  }
-};
+	initialized = true
+	if (
+		'onGatsbyInitialClientRender' in window &&
+		typeof window.onGatsbyInitialClientRender === 'function'
+	) {
+		window.onGatsbyInitialClientRender()
+	}
+	if (
+		'onGatsbyRouteUpdate' in window &&
+		typeof window.onGatsbyRouteUpdate === 'function'
+	) {
+		window.onGatsbyRouteUpdate()
+	}
+}
 
 exports.onRouteUpdate = () => {
-  if (
-    initialized &&
-    "onGatsbyRouteUpdate" in window &&
-    typeof window.onGatsbyRouteUpdate === "function"
-  ) {
-    window.onGatsbyRouteUpdate();
-  }
-};
+	if (
+		initialized &&
+		'onGatsbyRouteUpdate' in window &&
+		typeof window.onGatsbyRouteUpdate === 'function'
+	) {
+		window.onGatsbyRouteUpdate()
+	}
+}
 
 exports.onPreRouteUpdate = () => {
-  if (
-    initialized &&
-    "onGatsbyPreRouteUpdate" in window &&
-    typeof window.onGatsbyPreRouteUpdate === "function"
-  ) {
-    window.onGatsbyPreRouteUpdate();
-  }
-};
+	if (
+		initialized &&
+		'onGatsbyPreRouteUpdate' in window &&
+		typeof window.onGatsbyPreRouteUpdate === 'function'
+	) {
+		window.onGatsbyPreRouteUpdate()
+	}
+}
