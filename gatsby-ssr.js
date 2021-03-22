bd95294f63bea0c02c3f6cf1c9530d590e4a4103
killshot13 <dmreh@outlook.com>
2021-03-22 05:32:58 -0500

    added full dark theme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,17 +4,17 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require("react");
-const withPrefix = require("./src/utils/withPrefix").default;
+const React = require('react')
+const withPrefix = require('./src/utils/withPrefix').default
 
 exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
-  setHeadComponents([]);
+	setHeadComponents([])
 
-  setPostBodyComponents([
-    <React.Fragment>
-      <script src={withPrefix("js/init.js")} />
-      <script src={withPrefix("js/page-load.js")} />
-      <script src={withPrefix("js/page-unload.js")} />
-    </React.Fragment>,
-  ]);
-};
+	setPostBodyComponents([
+		<React.Fragment>
+			<script src={withPrefix('js/init.js')} />
+			<script src={withPrefix('js/page-load.js')} />
+			<script src={withPrefix('js/page-unload.js')} />
+		</React.Fragment>,
+	])
+}
