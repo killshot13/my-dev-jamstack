4191512819181cd6ddb23ba3bfc8fa35db42c452
killshot13 <dmreh@outlook.com>
2021-03-28 21:07:07 -0500

    fixed layout issues with blockquotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,23 @@ module.exports = {
 		`gatsby-plugin-advanced-sitemap`,
 		`gatsby-plugin-react-helmet`,
 		`gatsby-source-data`,
-		`gatsby-transformer-remark`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth: 1200,
+						},
+					},
+				],
+			},
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
