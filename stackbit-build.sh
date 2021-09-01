62664c1754217e56001b016f2f6234205cff5118
Michael Rehnert (killshot13) <dmreh@outlook.com>
2021-09-01 05:01:57 -0500

    Update(mdj): restructured npm packages

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,9 +7,9 @@ set -v
 echo "stackbit-build.sh: start build"
 
 # fetch data from CMS through stackbit-pull
-npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60323df295aa610015b59f66 --stackbit-api-key="bd48a94d051a6ed548c6627e74e2075f911d90abe3b7ded6ca5f0249aa5ce996"
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/60323df295aa610015b59f66 --stackbit-api-key=STACKBIT_API_KEY
 
 # build site
-npm run build
+yarn run build
 
 echo "stackbit-build.sh: finished build"
